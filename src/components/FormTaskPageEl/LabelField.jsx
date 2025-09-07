8e19a664521dffe08874134ca616aae0cba01e89
Bohdan <bohdan.verbetskyi@gmail.com>
2025-09-07 13:41:16 +0200

    Fix name styles

--- src/components/FormTaskPageEl/LabelField.jsx
+++ src/components/FormTaskPageEl/LabelField.jsx
@@ -3,11 +3,15 @@ import { Field, ErrorMessage } from 'formik'
 import styles from './LabelField.module.scss'
 
 export default function LabelField({ label, name, type = 'text' }) {
-	return (
-		<div className={styles.fieldContainer}>
-			<label htmlFor={name}>{label}</label>
-			<Field id={name} name={name} type={type} />
-			<ErrorMessage name={name} component='div' className={styles.error} />
-		</div>
-	)
+    return (
+        <div className={styles.fieldContainer}>
+            <label htmlFor={name}>{label}</label>
+            <Field id={name} name={name} type={type} />
+            <ErrorMessage
+                name={name}
+                component="div"
+                className={styles.error}
+            />
+        </div>
+    )
 }
--- src/components/FormTaskPageEl/TaskForm.jsx
+++ src/components/FormTaskPageEl/TaskForm.jsx
@@ -5,34 +5,38 @@ import VALIDATION_SCHEMA_TASK from '../../utils/validationSchemaTask'
 import LabelField from '../FormTaskPageEl/LabelField'
 
 export default function TaskForm({
-	initialValues,
-	onSubmit,
-	submitText,
-	onCancel,
+    initialValues,
+    onSubmit,
+    submitText,
+    onCancel,
 }) {
-	return (
-		<Formik
-			initialValues={initialValues}
-			onSubmit={onSubmit}
-			validationSchema={VALIDATION_SCHEMA_TASK}
-		>
-			<Form className={styles.taskForm}>
-				<LabelField label='Task Name' name='name' />
-				<LabelField label='Deadline' name='lastDateToRealization' type='date' />
+    return (
+        <Formik
+            initialValues={initialValues}
+            onSubmit={onSubmit}
+            validationSchema={VALIDATION_SCHEMA_TASK}
+        >
+            <Form className={styles.taskForm}>
+                <LabelField label="Task Name" name="name" />
+                <LabelField
+                    label="Deadline"
+                    name="lastDateToRealization"
+                    type="date"
+                />
 
-				<div className={styles.formBtns}>
-					<button type='submit'>{submitText}</button>
-					{onCancel && (
-						<button
-							type='button'
-							className={styles.cancelBtn}
-							onClick={onCancel}
-						>
-							Cancel
-						</button>
-					)}
-				</div>
-			</Form>
-		</Formik>
-	)
+                <div className={styles.formBtns}>
+                    <button type="submit">{submitText}</button>
+                    {onCancel && (
+                        <button
+                            type="button"
+                            className={styles.cancelBtn}
+                            onClick={onCancel}
+                        >
+                            Cancel
+                        </button>
+                    )}
+                </div>
+            </Form>
+        </Formik>
+    )
 }
